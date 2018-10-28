de227cb162164e39dc38a277443e0ad8b10a0c13
Anderson Oliveira <andersonoliveira@MacBook-Pro-de-Anderson.local>
2018-10-28 09:07:58 -0300

    Add project

---
+++ Gitz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Gitz
+//
+//  Created by Anderson Oliveira on 27/10/18.
+//  Copyright © 2018 Anderson Oliveira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
